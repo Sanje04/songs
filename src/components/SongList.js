a499dcae36df6e80142b5774f2de1407c985fc8e
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-07-20 22:41:47 -0400

    building a list with redux data

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,12 +2,40 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends React.Component {
+    //take list of songs, map over them and give back as jsx
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <div className="ui button primary">
+                            Select
+                        </div>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        })
+    }
+
     render() {
+        //console.log(this.props.songs);
         return (
-            <div>SongList</div>
+            <div className="ui divided list">{this.renderList()}</div>
         )
     }
 }
 
+//state has all the data inside the redux store
+
+
+const mapStateToProps = (state) => {
+    //console.log(state);
+
+    return {
+        songs: state.songs
+    };
+}
+
 //second set of parenthesis invoke the function being returned
-export default connect()(SongList);
+export default connect(mapStateToProps)(SongList);
